5f64e5d03a16351d51c51e6a116db83018a7f714
Kanstantsin Kiyko <xfozzyx@gmail.com>
2018-02-21 17:06:37 +0300

    12 tests pass

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let splited = str.split('');
+  let brackets = bracketsConfig.reduce(function(a, b) {
+    return a.concat(b);
+  }, []);
+  let stack = [];
+  const opening = brackets.filter((e, i) => !(i % 2));
+  for (let i = 0; i < splited.length; i++) {
+    if (brackets.includes(splited[i])) {
+      if (opening.includes(splited[i])) {
+        stack.push(splited[i]);
+      } else {
+        let last = stack.pop();
+        if (brackets.indexOf(splited[i]) - 1 === last) {
+          return false;
+        }
+      }
+    }
+  }
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
